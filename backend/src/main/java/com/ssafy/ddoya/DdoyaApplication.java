192a75915610f4e1155e349f9075ae5ee6c39e19
yuj130605 <yuj130605@gmail.com>
2026-03-11 11:22:48 +0900

    feat: Spring Boot 백엔드 프로젝트 초기 생성 및 도메인 기반 패키지 구조 구성

---
+++ backend/src/main/java/com/ssafy/ddoya/DdoyaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ddoya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdoyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdoyaApplication.class, args);
+	}
+
+}
