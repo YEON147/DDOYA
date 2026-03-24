112f7d55bf738c3dd149eaccd674ff0ab02cbf59
goring <dlrkdms001@gmail.com>
2026-03-24 16:31:15 +0900

    fix: CI/CD 테스트

--- backend/src/main/java/com/ssafy/ddoya/global/config/RestTemplateConfig.java
+++ backend/src/main/java/com/ssafy/ddoya/global/config/RestTemplateConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 import java.time.Duration;
 
 @Configuration
-public class RestTemplateConfig {
+public class RestTemplateConfig { 
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
